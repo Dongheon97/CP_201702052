2dd5c2d491f028faecc9f6627ceb79c9ce192bab
Dongheon97 <jdd04063@gmail.com>
2017-11-17 16:34:12 +0900

    1st assignment

---
+++ 이동헌_201702052_WO1-3/src/Report1x3.java
@@ -0,0 +1,74 @@
+
+public class Report1x3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lineNumber;
+		
+		lineNumber=1;
+		System.out.println(lineNumber + ". 창 밖에 밤비가 속살거려");
+		lineNumber=2;
+		System.out.println(lineNumber + ". 육첩방(六疊房)은 남의 나라.");
+		lineNumber=3;
+		System.out.println(lineNumber + ". ");
+		lineNumber=4;
+		System.out.println(lineNumber + ". 시인이란 슬픈 천명(天命)인 줄 알면서도");
+		lineNumber=5;
+		System.out.println(lineNumber + ". 한 줄 시를 적어 볼까.");
+		lineNumber=6;
+		System.out.println(lineNumber + ". ");
+		lineNumber=7;
+		System.out.println(lineNumber + ". 땀내와 사랑내 포근히 품긴");
+		lineNumber=8;
+		System.out.println(lineNumber + ". 보내 주신 학비 봉투를 받아");
+		lineNumber=9;
+		System.out.println(lineNumber + ". ");
+		lineNumber=10;
+		System.out.println(lineNumber + ". 대학 노트를 끼고");
+		lineNumber=11;
+		System.out.println(lineNumber + ". 늙은 교수의 강의를 들으러 간다.");
+		lineNumber=12;
+		System.out.println(lineNumber + ". ");
+		lineNumber=13;
+		System.out.println(lineNumber + ". 생각해 보면 어린 때 동무들");
+		lineNumber=14;
+		System.out.println(lineNumber + ". 하나, 둘, 죄다 잃어버리고");
+		lineNumber=15;
+		System.out.println(lineNumber + ". ");
+		lineNumber=16;
+		System.out.println(lineNumber + ". 나는 무얼 바라");
+		lineNumber=17;
+		System.out.println(lineNumber + ". 나는 다만, 홀로 침전(沈澱)하는 것일까?");
+		lineNumber=18;
+		System.out.println(lineNumber + ". ");
+		lineNumber=19;
+		System.out.println(lineNumber + ". 인생은 살기 어렵다는데");
+		lineNumber=20;
+		System.out.println(lineNumber + ". 시가 이렇게 쉽게 쓰여지는 것은");
+		lineNumber=21;
+		System.out.println(lineNumber + ". 부끄러운 일이다.");
+		lineNumber=22;
+		System.out.println(lineNumber + ". ");
+		lineNumber=23;
+		System.out.println(lineNumber + ". 육첩방은 남의 나라");
+		lineNumber=24;
+		System.out.println(lineNumber + ". 창밖에 밤비가 속살거리는데,");
+		lineNumber=25;
+		System.out.println(lineNumber + ". ");
+		lineNumber=26;
+		System.out.println(lineNumber + ". 등불을 밝혀 어둠을 조금 내몰고,");
+		lineNumber=27;
+		System.out.println(lineNumber + ". 시대처럼 올 아침을 기다리는 최후의 나.");
+		lineNumber=28;
+		System.out.println(lineNumber + ". ");
+		lineNumber=29;
+		System.out.println(lineNumber + ". 나는 나에게 작은 손을 내밀어");
+		lineNumber=30;
+		System.out.println(lineNumber + ". 눈물과 위안으로 잡는 최초의 악수.");
+		lineNumber=31;
+		System.out.println(lineNumber + ". ");
+		lineNumber=32;
+		System.out.println(lineNumber + ". - 쉽게 쓰여진 시 (윤동주) - ");
+	}
+
+}
